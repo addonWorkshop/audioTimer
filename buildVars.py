501542c40e1c2e8859b97003887df246c467328c
Danstiv <50794055+Danstiv@users.noreply.github.com>
2026-05-29 00:20:46 +0700

    Update addon URL

--- buildVars.py
+++ buildVars.py
@@ -29,7 +29,7 @@ addon_info = {
 	# Author(s)
 	"addon_author": "Danstiv <danstiv@yandex.ru>",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/danstiv/audioTimer",
+	"addon_url": "https://github.com/addonWorkshop/audioTimer",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
